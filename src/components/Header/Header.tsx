9509d6b1234cad9cc507cfc678e7bb4e49f816c1
junior <junior.oliveira05@icloud.com>
2025-03-14 10:06:25 -0300

    feat: adiciona header à página

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,36 @@
 import styles from './Header.module.scss';
 import Logo from '../../assets/logo.png'
 import { FaRegUserCircle } from "react-icons/fa";
-import { CiHeart } from "react-icons/ci";
+import { CiHeart, CiCreditCard1 } from "react-icons/ci";
 import { PiShoppingCart } from "react-icons/pi";
 import { MdAssignmentReturn } from "react-icons/md";
+import { RiTruckLine } from "react-icons/ri";
+import { IoShieldCheckmarkOutline } from "react-icons/io5";
 
 export function Header() {
   return (
     <header className={styles.header}>
+
+      <div className={styles.topBar}>  
+        <span><IoShieldCheckmarkOutline className={styles.iconeTopBar} /> Compra <strong>100% segura</strong></span>
+        <span><RiTruckLine className={styles.iconeTopBar}  /> Frete grátis acima de <strong>R$ 200</strong></span>
+        <span><CiCreditCard1  className={styles.iconeTopBar}  /> <strong>Parcele</strong> suas compras</span>
+      </div>
+
       <div className={styles.container}>
         {/* Logo */}
         <div className={styles.logo}>
           <img src={Logo} alt="Logo da Loja" />
         </div>
 
-        {/* Menu de navegação */}
-        <nav className={styles.nav}>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Produtos</a></li>
-            <li><a href="#">Promoções</a></li>
-            <li><a href="#">Contato</a></li>
-          </ul>
-        </nav>
+        <div>
+          <input type="text" placeholder='O que está buscando?'/>
+        </div>
 
         {/* Ícones (Busca, Conta, Carrinho) */}
         <div className={styles.icons}>
         <button title='compras'><MdAssignmentReturn /></button>
-        <button title='gostei'><CiHeart /></button>
+        <button title='favoritos'><CiHeart /></button>
           <button title='pesquisa'><FaRegUserCircle /></button>
           <button title='carrinho'><PiShoppingCart /></button>
         </div>
